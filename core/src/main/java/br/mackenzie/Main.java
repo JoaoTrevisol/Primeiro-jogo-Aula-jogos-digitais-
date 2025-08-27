fb9ed39aadfb83a81dc7de2fe6755427a1aadeb5
Joao Trevisol <joao.trevisa@gmail.com>
2025-08-27 19:51:22 -0300

    Criação do boneco bucket e definição de viewport e mover o balde para a esquerda e para a direita com o teclado.

---
+++ core/src/main/java/br/mackenzie/Main.java
@@ -0,0 +1,88 @@
+package br.mackenzie;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+    Texture bucketTexture;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+    Sprite bucketSprite;
+    @Override
+    public void create() {
+        bucketTexture = new Texture("bucket.png");
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8,5);
+
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1,1);
+        // Prepare your application here.
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); //true centers the camera
+        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
+        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
+        if(width <= 0 || height <= 0) return;
+
+        // Resize your application here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+        // Draw your application here.
+    }
+
+    private void input() {
+        float speed =  .65f;
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            bucketSprite.translateX(speed * delta); //Move o bucket right
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+
+        }
+
+    }
+
+    private void logic(){
+
+    }
+
+    private void draw(){
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+        bucketSprite.draw(spriteBatch);
+        spriteBatch.end();
+
+    }
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy application's resources here.
+    }
+}
